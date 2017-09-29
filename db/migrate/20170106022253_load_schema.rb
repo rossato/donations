bf6aa82beb0a2d07ce8018dea155b5bd06ba6fa3
Ken <rossato@users.noreply.github.com>
2017-09-29 10:11:33 -0400

    Fix db migration error

--- db/migrate/20170106022253_load_schema.rb
+++ db/migrate/20170106022253_load_schema.rb
@@ -1,4 +1,4 @@
-class LoadSchema < ActiveRecord::Migration
+class LoadSchema < ActiveRecord::Migration[5.0]
   def change
     create_table "donations", force: :cascade do |t|
       t.integer  "donor_id"
